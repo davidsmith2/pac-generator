49bd95ac8967fe1e1a7e63c3c2a10d14d0386bc2
David Smith <davidsmith2@gmail.com>
2015-02-03 12:56:16 -0500

    install Bootstrap Growl

--- frontend/js/common/views/alert.js
+++ frontend/js/common/views/alert.js
@@ -2,11 +2,15 @@ var Backbone = require('backbone');
 var Marionette = require('marionette');
 var $ = require('jquery');
 var _ = require('underscore');
+require('bootstrap-growl');
 
 module.exports =  Marionette.LayoutView.extend({
     template: require('./templates/alert.hbs'),
     className: 'alert',
     regions: {
         contentRegion: '.alert-content-region'
+    },
+    onBeforeRender: function () {
+        console.log($.growl)
     }
 });
