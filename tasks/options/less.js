f7f199e257bc4ab4a756e36d99b10a185556f26b
davidsmith2 <davidsmith2@gmail.com>
2015-02-15 13:18:50 -0500

    enable LESS source maps

--- tasks/options/less.js
+++ tasks/options/less.js
@@ -1,5 +1,11 @@
 module.exports = {
     build: {
+        options: {
+            sourceMap: true,
+            sourceMapFilename: 'build/css/index.css.map',
+            sourceMapURL: '/css/index.css.map',
+            outputSourceFiles: true
+        },
         files: {
             'build/css/index.css': ['frontend/css/index.less']
         }
