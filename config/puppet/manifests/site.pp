3ba47b4108d6b631d0d2158292784033d0798494
davidsmith2 <davidsmith2@gmail.com>
2015-03-12 22:16:04 -0400

    standing up librarian-puppet

--- config/puppet/manifests/site.pp
+++ config/puppet/manifests/site.pp
@@ -5,11 +5,7 @@ File {
     group   => 'root',
     mode    => '0644',
 }
-
 Exec {
     path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
 }
-
 include baseconfig
-include my_nodejs
-include my_apache
