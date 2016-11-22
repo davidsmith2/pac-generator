c54d3b2bc83b7787247d0171780409c70a3cc70e
David Smith <davidsmith2@gmail.com>
2016-11-22 18:46:05 -0500

    Try deploying to Beanstalk

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,7 @@
 SHA1=$1
 AWS_ACCESS_KEY_ID=$2
 AWS_SECRET_ACCESS_KEY=$3
-DOCKER_EMAIL=$4
-DOCKER_USERNAME=$5
-DOCKER_PASSWORD=$6
 
-# login
-docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-
-# push
-docker-compose -f docker-compose.production.yml push
-
-# deploy
 ./node_modules/.bin/grunt deploy \
 	--SHA1="$SHA1" \
 	--AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
