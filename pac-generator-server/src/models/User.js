2b2d685661a15983a0d19cc0b753ca625e25c345
David Smith <davidsmith2@gmail.com>
2016-11-27 18:38:44 -0500

    PAC file should output empty array if user has no rules or exceptions (fixes #25)

--- pac-generator-server/src/models/User.js
+++ pac-generator-server/src/models/User.js
@@ -1,11 +1,13 @@
 var bcrypt = require('bcrypt-nodejs');
 var fs = require('fs');
+var mkdirp = require('mkdirp');
 var mongoose = require('mongoose');
 var _ = require('underscore');
 
 var ExceptionSchema = require('./ExceptionSchema');
 var ProxySchema = require('./ProxySchema');
 var RuleSchema = require('./RuleSchema');
+var PACTemplate = require('../templates/pac');
 
 var UserSchema = mongoose.Schema({
     _id: String,
@@ -28,15 +30,17 @@ UserSchema.methods.validPassword = function (password) {
 };
 
 UserSchema.methods.writePAC = function (proxy) {
-    var mkdirp = require('mkdirp');
     var dirp = './dist/pac/' + this.uuid + '/' + proxy.id;
-    var self = this;
     mkdirp(dirp, _.bind(function (err) {
         var data;
         if (err) {
             return console.log(err);
         }
-        data = require('../templates/pac')(this, proxy);
+        data = PACTemplate(
+            proxy,
+            this.getPACHosts(this.exceptions),
+            this.getPACHosts(this.rules)
+        );
         fs.writeFile(dirp + '/proxy.pac', data, function (err) {
             if (err) {
                 return console.log(err);
@@ -44,6 +48,16 @@ UserSchema.methods.writePAC = function (proxy) {
             console.log('PAC file created for ' + proxy.name + ' proxy');
         });
     }, this));
+};
+
+UserSchema.methods.getPACHosts = function (arrIn) {
+    var arrOut = [];
+    for (var i = 0; i < arrIn.length; i++) {
+        if (arrIn[i].active) {
+            arrOut.push(arrIn[i].host);
+        }
+    }
+    return (arrOut.length) ? "'" + arrOut.join("','") + "'" : arrOut;
 };
 
 module.exports = mongoose.model('User', UserSchema);
--- pac-generator-server/src/templates/pac.js
+++ pac-generator-server/src/templates/pac.js
@@ -1,33 +1,20 @@
-var getHosts = function (arrIn) {
-    var arrOut = [];
-    for (var i = 0; i < arrIn.length; i++) {
-        if (arrIn[i].active) {
-            arrOut.push(arrIn[i].host);
-        }
-    }
-    return "'" + arrOut.join("','") + "'";
-};
-
-module.exports = function (user, proxy) {
-    var server = proxy.server + ':'  + proxy.port;
-    var exceptions = getHosts(user.exceptions);
-    var rules = getHosts(user.rules);
-    var output = '';
-    output += "function FindProxyForURL(url, host) {\n";
-    output += "\tvar proxy = '" + server + "';\n";
-    output += "\tvar exceptions = [" + exceptions + "];\n";
-    output += "\tvar rules = [" + rules + "];\n";
-    output += "\tfor (var i = 0; i < exceptions.length; i++) {\n";
-    output += "\t\tif (dnsDomainIs(host, exceptions[i])) {\n";
-    output += "\t\t\treturn 'DIRECT';\n";
-    output += "\t\t}\n";
-    output += "\t}\n";
-    output += "\tfor (var j = 0; j < rules.length; j++) {\n";
-    output += "\t\tif (dnsDomainIs(host, rules[j])) {\n";
-    output += "\t\t\treturn 'PROXY ' + proxy;\n";
-    output += "\t\t}\n";
-    output += "\t}\n";
-    output += "\treturn 'DIRECT';\n";
-    output += "}";
-    return output;
+module.exports = function (proxy, exceptions, rules) {
+    var str = '';
+    str += "function FindProxyForURL(url, host) {\n";
+    str += "\tvar proxy = '" + proxy.server + ":" + proxy.port + "';\n";
+    str += "\tvar exceptions = [" + exceptions + "];\n";
+    str += "\tvar rules = [" + rules + "];\n";
+    str += "\tfor (var i = 0; i < exceptions.length; i++) {\n";
+    str += "\t\tif (dnsDomainIs(host, exceptions[i])) {\n";
+    str += "\t\t\treturn 'DIRECT';\n";
+    str += "\t\t}\n";
+    str += "\t}\n";
+    str += "\tfor (var j = 0; j < rules.length; j++) {\n";
+    str += "\t\tif (dnsDomainIs(host, rules[j])) {\n";
+    str += "\t\t\treturn 'PROXY ' + proxy;\n";
+    str += "\t\t}\n";
+    str += "\t}\n";
+    str += "\treturn 'DIRECT';\n";
+    str += "}";
+    return str;
 };
