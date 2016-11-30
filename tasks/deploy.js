394b58b556c49f32543ca8a2d06b865faed4e679
David Smith <davidsmith2@gmail.com>
2016-11-29 20:53:51 -0500

    Fix build

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -1,6 +1,6 @@
 module.exports = function (grunt) {
     grunt.registerTask('deploy', [
-    	'exec:docker:' + grunt.option('SHA1') + ':' + 'common-services.yml'
+    	'exec:docker:' + grunt.option('SHA1') + ':' + 'common-services.yml',
         'clean',
         'zip'
     ]);
